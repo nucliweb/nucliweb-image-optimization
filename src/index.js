2049f09bbc33cd6b0ffe2918c2b6177d71ebfa58
Victor Bejas <victorbejas97@gmail.com>
2021-06-09 14:35:04 -0300

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,41 +1,32 @@
-const sharp = require("sharp");
-const fs = require("fs");
+const sharp = require('sharp')
+const fs = require('fs')
+const { formats } = require('./formats')
 
-const ORIGINALS = "./originals/";
-const IMAGES = "./images/";
-const formats = [
-  // jpeg
-  { width: 640, format: "jpg", suffix: "-s" },
-  { width: 768, format: "jpg", suffix: "-m" },
-  { width: 1024, format: "jpg", suffix: "-l" },
-  // webp
-  { width: 640, format: "webp", suffix: "-s" },
-  { width: 768, format: "webp", suffix: "-m" },
-  { width: 1024, format: "webp", suffix: "-l" },
-  // avif
-  { width: 640, format: "avif", suffix: "-s" },
-  { width: 768, format: "avif", suffix: "-m" },
-  { width: 1024, format: "avif", suffix: "-l" },
-];
+const ORIGINALS = './originals/'
+const IMAGES = './images/'
 
-fs.rmdirSync(IMAGES, { recursive: true });
-
-if (!fs.existsSync(IMAGES)) {
-  fs.mkdirSync(IMAGES);
-}
-
-fs.readdirSync(ORIGINALS).forEach((file) => {
-  const isImageFile = /\.(jpe?g|png|tif?f|avif)$/i.test(file);
-
+function convertToimg ({ isImageFile, file }) {
   if (isImageFile) {
     formats.map(({ width, format, suffix }) => {
-      const filename = file.split(".").slice(0, -1).join(".");
+      const filename = file.split('.').slice(0, -1).join('.')
 
-      sharp(`${ORIGINALS}/${file}`)
+      return sharp(`${ORIGINALS}/${file}`)
         .resize(width)
         .toFile(`${IMAGES}${filename}${suffix}.${format}`, () =>
           console.log(`${IMAGES}${filename}${suffix}.${format}`)
-        );
-    });
+        )
+    })
   }
-});
+}
+
+fs.rmdirSync(IMAGES, { recursive: true })
+
+if (!fs.existsSync(IMAGES)) {
+  fs.mkdirSync(IMAGES)
+}
+
+fs.readdirSync(ORIGINALS).forEach((file) => {
+  const isImageFile = /\.(jpe?g|png|tif?f|avif)$/i.test(file)
+
+  convertToimg({ isImageFile, file })
+})
